d5d86fbff3392a5fdc5f58ff88af5f5c4e450340
Adam Priest <sepone229@gmail.com>
2020-03-20 00:57:37 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,28 @@
-require 'pry'
-
 class String
-
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
-
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
-
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
-
   def count_sentences
-
+    new_array = self.split(/[.?!]/)
+    new_array.delete_if{|sentence| sentence.empty?}
+    new_array.count
   end
 end
