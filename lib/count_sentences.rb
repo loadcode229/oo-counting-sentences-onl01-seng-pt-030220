23559d7c85c9045511b64d24cf342feeaf308088
Adam Priest <sepone229@gmail.com>
2020-03-20 00:47:36 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -28,8 +28,7 @@ class String
 
   def count_sentences
     new_array = self.split(/[.?!]/)
-    new_array.delete_if?{|sentence| sentence.empty?}
+    new_array.delete_if{|sentence| sentence.empty?}
     new_array.count
-    binding.pry
   end
 end
